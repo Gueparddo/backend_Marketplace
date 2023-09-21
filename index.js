d724a82fab7f5274ed82190a3eb45a55eab63f23
EstebGtzMtz <esteb.gtz.mtz@hotmail.com>
2023-09-21 16:24:49 -0600

    add gitignore file and callback to show that the server is running in the correct port

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hola');
+const express = require('express')
+const app = express()
+
+const PORT = 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(PORT,()=>{
+  console.log(`server running in port ${300} 🚀🤘🏼`)
+})
