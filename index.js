72d131f5185360a99b0955a497575097eae3571e
Moi Muñoz <moims2197@gmail.com>
2023-09-21 17:02:32 -0600

    Servidor vivo, creación de los primeros dos end points

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const app = express()
 
 const PORT = 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.post('/perro', function (require, response) {
+  response.send('Hello World')
+})
+
+app.get('/', function(req,response){
+    response.send("Estamos alive")
 })
 
 app.listen(PORT,()=>{
-  console.log(`server running in port ${PORT} 🚀🤘🏼`)
+    console.log(`server running in port ${PORT} 🚀🤘🏼`)
 })
